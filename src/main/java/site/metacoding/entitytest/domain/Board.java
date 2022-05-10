bf7f6d5d950efc446383c39ef533d73f4958d1df
AR <aki1013@naver.com>
2022-05-10 15:36:25 +0900

    QLRM까지 사용 완료 - 네이티브쿼리 동작 마스터함

--- src/main/java/site/metacoding/entitytest/domain/Board.java
+++ src/main/java/site/metacoding/entitytest/domain/Board.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.xml.crypto.KeySelector.Purpose;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
